6638c1779c85581299ab82ddd86850fdf611a939
Hemant Rathore <your-email@gmail.com>
2026-09-20 18:11:48 +0530

    Update portfolio backend

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.use((error, req, res, next) => {
   return next(error);
 });
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 5001;
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on port ${PORT}`);
